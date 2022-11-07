546dc30513867574c6db41534d8037807199c39d
Alfred <59073967+slimer37@users.noreply.github.com>
2022-11-06 20:28:06 -0800

    Make Interact method abstract

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -9,7 +9,8 @@ public abstract class Interactable : MonoBehaviour
     }
 
     public virtual string GetText() => "Interact";
-    public virtual void Interact() => Debug.Log("interact");
     public virtual bool CanInteract() => true;
+
+    public abstract void Interact();
     public virtual void OnHover(bool hover) => Debug.Log("hover " + hover);
 }
