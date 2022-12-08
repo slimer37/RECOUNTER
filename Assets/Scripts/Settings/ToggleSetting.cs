6f02c895f693926ab8e139f351906b645fa79ed0
Alfred <59073967+slimer37@users.noreply.github.com>
2022-12-08 01:59:33 -0800

    Add bob toggle to settings

---
+++ Assets/Scripts/Settings/ToggleSetting.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class ToggleSetting : MonoBehaviour
+{
+    [SerializeField] string key;
+    [SerializeField] bool defaultValue;
+
+    void Awake()
+    {
+        var toggle = GetComponent<Toggle>();
+        toggle.isOn = PlayerPrefs.GetInt(key, defaultValue ? 1 : 0) > 0;
+        toggle.onValueChanged.AddListener(OnToggle);
+    }
+
+    void OnToggle(bool value)
+    {
+        PrefManager.SetInt(key, value ? 1 : 0);
+    }
+}
