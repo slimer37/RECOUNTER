acb2bfe67063fca05ab4771b484df2f0406107be
Alfred <59073967+slimer37@users.noreply.github.com>
2022-12-14 00:39:05 -0800

    Basic door

---
+++ Assets/Scripts/Interactables/Door.cs
@@ -0,0 +1,58 @@
+using NaughtyAttributes;
+using UnityEngine;
+
+public class Door : Interactable
+{
+    [SerializeField] float _pullPointDistance;
+    [SerializeField] float _pullStrength;
+    [SerializeField] float _smoothing;
+    [SerializeField, Required] Transform _rotator;
+    [SerializeField] Vector3 _pullDirection = Vector3.forward;
+    [SerializeField] Vector3 _rotationAxis = Vector3.up;
+    [SerializeField] float _deceleration;
+
+    float _rotation;
+    float _rotationVelocity;
+
+    protected override bool CanInteract(Employee e) => !IsInteractionInProgress;
+
+    public override HudInfo GetHudInfo(Employee e) => CanInteract(e)
+        ? new HudInfo { icon = Icon.Door, text = "Push or Pull" }
+        : BlankHud;
+
+    [Button("Normalize Axes")]
+    void NormalizeAxis()
+    {
+        _rotationAxis.Normalize();
+        _pullDirection.Normalize();
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(_rotator.position, _rotationAxis);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(_rotator.position, _rotator.TransformDirection(_pullDirection));
+    }
+
+    void Awake()
+    {
+        _rotation = Vector3.Dot(_rotator.eulerAngles, _rotationAxis);
+    }
+
+    void Update()
+    {
+        if (IsInteractionInProgress)
+        {
+            var pullPoint = Interactor.transform.TransformPoint(Vector3.forward * _pullPointDistance);
+            var pull = Vector3.Dot(pullPoint - _rotator.position, _rotator.TransformDirection(_pullDirection));
+            _rotationVelocity = pull * _pullStrength * Time.deltaTime;
+        }
+
+        _rotationVelocity = Mathf.MoveTowards(_rotationVelocity, 0, _deceleration * Time.deltaTime);
+
+        _rotation += _rotationVelocity;
+
+        _rotator.eulerAngles = _rotationAxis * _rotation;
+    }
+}
