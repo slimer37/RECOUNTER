efcb993282f4a1e94e264fb081f6afacf5b5de34
Alfred <59073967+slimer37@users.noreply.github.com>
2022-12-15 00:21:17 -0800

    Physics-based door, courtesy of AI ChatGPT

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -7,12 +7,9 @@ public class Door : Interactable
     [SerializeField] float _pullStrength;
     [SerializeField, Required] Transform _rotator;
     [SerializeField] Vector3 _pullDirection = Vector3.forward;
-    [SerializeField] Vector3 _rotationAxis = Vector3.up;
     [SerializeField] float _deceleration;
-    [SerializeField, MinMaxSlider(-180, 180)] Vector2 _limits = new(-180, 180);
 
-    float _rotation;
-    float _rotationVelocity;
+    HingeJoint _hinge;
 
     protected override bool CanInteract(Employee e) => !IsInteractionInProgress;
 
@@ -23,40 +20,40 @@ public class Door : Interactable
     [Button("Normalize Axes")]
     void NormalizeAxis()
     {
-        _rotationAxis.Normalize();
         _pullDirection.Normalize();
     }
 
     void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.green;
-        Gizmos.DrawRay(_rotator.position, _rotationAxis);
         Gizmos.color = Color.blue;
         Gizmos.DrawRay(_rotator.position, _rotator.TransformDirection(_pullDirection));
     }
 
     void Awake()
     {
-        _rotation = Vector3.Dot(_rotator.eulerAngles, _rotationAxis);
+        _hinge = GetComponent<HingeJoint>();
     }
 
     void Update()
     {
+        var rotationVelocity = 0f;
+
         if (IsInteractionInProgress)
         {
             var pullPoint = Interactor.transform.TransformPoint(Vector3.forward * _pullPointDistance);
             var pullDir = _rotator.TransformDirection(_pullDirection);
             var pull = Vector3.Dot(pullPoint - _rotator.position, pullDir);
 
-            _rotationVelocity = pull * _pullStrength * Time.deltaTime;
+            rotationVelocity = pull * _pullStrength;
         }
 
-        _rotationVelocity = Mathf.MoveTowards(_rotationVelocity, 0, _deceleration * Time.deltaTime);
+        Rotate(rotationVelocity);
+    }
 
-        _rotation += _rotationVelocity;
-
-        _rotation = Mathf.Clamp(_rotation, _limits.x, _limits.y);
-
-        _rotator.eulerAngles = _rotationAxis * _rotation;
+    void Rotate(float speed)
+    {
+        var motor = _hinge.motor;
+        motor.targetVelocity = speed;
+        _hinge.motor = motor;
     }
 }
