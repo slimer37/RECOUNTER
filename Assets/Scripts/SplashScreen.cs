3532974671a93b799152dc2cfa7558506afc7eb0
Alfred <59073967+slimer37@users.noreply.github.com>
2022-12-15 19:49:01 -0800

    Expand splash screen functionality and edit splash

--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -1,26 +1,63 @@
 using DG.Tweening;
+using NaughtyAttributes;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SplashScreen : MonoBehaviour
 {
-    [SerializeField] float fadeDuration;
-    [SerializeField] float duration;
+    [Serializable]
+    struct Splash
+    {
+        [SerializeField] CanvasGroup group;
+        [SerializeField] float duration;
+        [SerializeField] float fadeDuration;
+
+        public Tween GetTween(object target)
+        {
+            group.alpha = 0;
+
+            return DOTween.Sequence(target)
+            .Append(group.DOFade(1, fadeDuration))
+            .AppendInterval(duration)
+            .Append(group.DOFade(0, fadeDuration));
+        }
+    }
+
+    [SerializeField] float finalFadeOut;
     [SerializeField] CanvasGroup background;
-    [SerializeField] CanvasGroup group;
+    [SerializeField] Splash[] splashes;
+
+    [Header("Components")]
     [SerializeField] Canvas canvas;
+    [SerializeField] GraphicRaycaster raycaster;
 
     void Awake()
     {
         canvas.enabled = true;
 
         background.alpha = 1;
-        group.alpha = 0;
 
-        DOTween.Sequence()
-            .Append(group.DOFade(1, fadeDuration))
-            .AppendInterval(duration)
-            .Append(group.DOFade(0, fadeDuration))
-            .Join(background.DOFade(0, fadeDuration))
-            .OnComplete(() => canvas.enabled = false);
+        var sequence = DOTween.Sequence(this);
+
+        foreach (var splash in splashes)
+        {
+            sequence.Append(splash.GetTween(this));
+        }
+
+        sequence
+            .AppendCallback(OnLogosFinished)
+            .Append(background.DOFade(0, finalFadeOut))
+            .AppendCallback(OnSplashScreenFinished);
+    }
+
+    void OnLogosFinished()
+    {
+        raycaster.enabled = false;
+    }
+
+    void OnSplashScreenFinished()
+    {
+        canvas.enabled = false;
     }
 }
