6170085fe62bb07c990b95f5123631dcdb38fa16
Alfred <59073967+slimer37@users.noreply.github.com>
2022-12-28 20:17:46 -0800

    Add pull switch to neon sign

---
+++ Assets/Scripts/Interactables/PoweredSwitch.cs
@@ -0,0 +1,99 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PoweredSwitch : Interactable
+{
+    [SerializeField] PowerInlet inlet;
+    [SerializeField] HudInfo interactHud;
+
+    [Header("Animation")]
+    [SerializeField] bool animate;
+    [SerializeField] bool punch;
+    [SerializeField] Transform tweenTarget;
+    [SerializeField] Vector3 scaleAmount;
+    [SerializeField] Vector3 moveAmount;
+    [SerializeField] float duration;
+
+    public UnityEvent<bool> Switched;
+
+    public bool IsActuallyOn { get; private set; }
+    public bool IsSwitchedOn { get; private set; }
+
+    bool isAnimating;
+
+    void Awake()
+    {
+        inlet.StateChanged.AddListener(VerifyPowerState);
+    }
+
+    protected override bool CanInteract(Employee e) => !isAnimating;
+
+    public override HudInfo GetHudInfo(Employee e) => CanInteract(e)
+        ? interactHud
+        : BlankHud;
+
+    protected override void OnInteract(Employee e)
+    {
+        Switch(!IsSwitchedOn);
+
+        Animate();
+    }
+
+    void VerifyPowerState(bool powered)
+    {
+        if (powered && IsSwitchedOn)
+        {
+            Set(true);
+        }
+        else if (!powered && IsSwitchedOn)
+        {
+            Set(false);
+        }
+    }
+
+    void Switch(bool on)
+    {
+        if (IsSwitchedOn == on) return;
+
+        IsSwitchedOn = on;
+
+        if (on && !inlet.IsPluggedIn) return;
+
+        Set(on);
+    }
+
+    void Set(bool on)
+    {
+        IsActuallyOn = on;
+
+        Switched?.Invoke(on);
+    }
+
+    void Animate()
+    {
+        if (!animate) return;
+
+        isAnimating = true;
+
+        var scale = scaleAmount;
+        var move = moveAmount;
+
+        if (!punch && !IsSwitchedOn)
+        {
+            scale *= -1;
+            move *= -1;
+        }
+
+        if (punch)
+        {
+            tweenTarget.DOPunchPosition(move, duration).SetRelative();
+            tweenTarget.DOPunchScale(scale, duration).SetRelative().OnComplete(() => isAnimating = false);
+        }
+        else
+        {
+            tweenTarget.DOLocalMove(move, duration).SetRelative();
+            tweenTarget.DOScale(scale, duration).SetRelative().OnComplete(() => isAnimating = false);
+        }
+    }
+}
