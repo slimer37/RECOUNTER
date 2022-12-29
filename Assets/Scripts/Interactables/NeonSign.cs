117f7a89d814fd93bfe327738930a802514fc73a
Alfred <59073967+slimer37@users.noreply.github.com>
2022-12-28 22:08:04 -0800

    Neon sign hum

--- Assets/Scripts/Interactables/NeonSign.cs
+++ Assets/Scripts/Interactables/NeonSign.cs
@@ -1,11 +1,21 @@
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 
 public class NeonSign : MonoBehaviour
 {
     [SerializeField] Renderer[] signParts;
-    [SerializeField] float offIntensity;
+    [SerializeField] EventReference hum;
+    [SerializeField, Min(0.01f)] float fadeInSpeed = 1f;
+    [SerializeField, Min(0.01f)] float fadeOutSpeed = 1f;
 
     Color[] onColors;
+
+    float fadeTime;
+
+    bool isOn;
+
+    EventInstance humInstance;
 
     const string Emission = "_EmissionColor";
 
@@ -23,19 +33,48 @@ public class NeonSign : MonoBehaviour
         }
 
         Set(false);
+
+        humInstance = RuntimeManager.CreateInstance(hum);
+        RuntimeManager.AttachInstanceToGameObject(humInstance, transform);
+    }
+
+    void Update()
+    {
+        if (isOn && fadeTime > 1) return;
+        if (!isOn && fadeTime < 0) return;
+
+        fadeTime += Time.deltaTime * (isOn ? fadeInSpeed : -fadeOutSpeed);
+
+        SetIntensity(fadeTime);
     }
 
     public void Set(bool on)
     {
+        isOn = on;
+
+        if (on)
+            humInstance.start();
+        else
+            humInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+    void SetIntensity(float intensity)
+    {
+        intensity = Mathf.Clamp01(intensity);
+
         for (var i = 0; i < signParts.Length; i++)
         {
             var part = signParts[i];
             var color = onColors[i];
 
-            if (!on)
-                color *= offIntensity;
+            color *= intensity;
 
             part.material.SetColor(Emission, color);
         }
+    }
+
+    void OnDestroy()
+    {
+        humInstance.release();
     }
 }
