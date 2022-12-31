1b2fab0dba9256557e8b4fad35a935ac66bedebc
Alfred <59073967+slimer37@users.noreply.github.com>
2022-12-30 19:54:51 -0800

    3D rope rendering

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -1,31 +1,41 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Rope : MonoBehaviour
 {
-    [SerializeField] LineRenderer _lineRenderer;
-    [SerializeField] int _resolution;
+    [SerializeField] TubeRenderer _tubeRenderer;
+    [SerializeField] int _nodeCount;
     [SerializeField] float _length;
     [SerializeField] Joint _templateNode;
 
     Rigidbody[] _rigidbodies;
     Vector3[] _positions;
 
+
     void Awake()
     {
-        _lineRenderer.positionCount = _resolution;
-        _positions = new Vector3[_resolution];
         InitializeRigidbodies();
+
+        _positions = new Vector3[_nodeCount];
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < _nodeCount; i++)
+        {
+            _positions[i] = _rigidbodies[i].position;
+        }
+
+        _tubeRenderer.SetPositions(_positions);
     }
 
     void InitializeRigidbodies()
     {
-        _rigidbodies = new Rigidbody[_resolution];
+        _rigidbodies = new Rigidbody[_nodeCount];
 
-        var segmentLength = _length / _resolution;
+        var segmentLength = _length / _nodeCount;
         Joint previousJoint = null;
 
-        for (int i = 0; i < _resolution; i++)
+        for (int i = 0; i < _nodeCount; i++)
         {
             var joint = Instantiate(_templateNode, transform);
             var node = joint.transform;
@@ -35,27 +45,25 @@ public class Rope : MonoBehaviour
             var rb = node.GetComponent<Rigidbody>();
             _rigidbodies[i] = rb;
 
-            if (i > 0)
+            if (i == 0)
+            {
+                rb.isKinematic = true;
+            }
+            else
             {
                 previousJoint.connectedBody = rb;
             }
 
-            if (i < _resolution - 1)
+            if (i == _nodeCount - 1)
+            {
+                Destroy(joint);
+            }
+            else
             {
                 previousJoint = joint;
             }
         }
 
         _templateNode.gameObject.SetActive(false);
-    }
-
-    void Update()
-    {
-        for (var i = 0; i < _resolution; i++)
-        {
-            _positions[i] = _rigidbodies[i].position;
-        }
-
-        _lineRenderer.SetPositions(_positions);
     }
 }
---
+++ Assets/Scripts/TubeRenderer.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class TubeRenderer : MonoBehaviour
+{
+    [SerializeField, Min(1)] int _ringResolution = 4;
+    [SerializeField, Min(0.0001f)] float _radius = 0.1f;
+    [SerializeField] bool _worldSpace;
+    [SerializeField] Vector3[] _positions = new Vector3[2];
+    
+    Vector3[] _vertices;
+
+    int[] _triangles;
+
+    Mesh _ropeMesh;
+
+    MeshFilter _meshFilter;
+    
+    void Awake()
+    {
+        InitializeMesh();
+
+        SetPositions(_positions);
+    }
+
+    void InitializeMesh()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+
+        _ropeMesh = new Mesh
+        {
+            name = "Rope"
+        };
+
+        _meshFilter.mesh = _ropeMesh;
+    }
+
+    public void SetPositions(Vector3[] positions)
+    {
+        _positions = positions;
+
+        _vertices = new Vector3[positions.Length * _ringResolution];
+        _triangles = new int[(_vertices.Length - _ringResolution) * 6];
+
+        GenerateMesh();
+    }
+
+    void GenerateMesh()
+    {
+        for (var i = 0; i < _positions.Length; i++)
+        {
+            var position = _positions[i];
+
+            var vertIndex = i * _ringResolution;
+
+            var forward = Vector3.zero;
+
+            if (i > 0)
+            {
+                forward += position - _positions[i - 1];
+            }
+
+            if (i < _positions.Length - 1)
+            {
+                forward += _positions[i + 1] - position;
+            }
+
+            forward.Normalize();
+
+            var center = position;
+
+            if (_worldSpace)
+            {
+                center = transform.InverseTransformPoint(center);
+            }
+
+            for (int j = 0; j < _ringResolution; j++)
+            {
+                var theta = (float)j / _ringResolution * 2 * Mathf.PI;
+                var x = _radius * Mathf.Cos(theta);
+                var y = _radius * Mathf.Sin(theta);
+                var pos = Vector3.up * y + Vector3.right * x;
+                var localPos = Quaternion.FromToRotation(Vector3.forward, forward) * pos;
+                _vertices[vertIndex + j] = center + localPos;
+            }
+
+            if (i == 0) continue;
+
+            var triIndex = (i - 1) * _ringResolution * 6;
+
+            for (int j = 0; j < _ringResolution - 1; j++)
+            {
+                _triangles[triIndex] = vertIndex;
+                _triangles[triIndex + 1] = vertIndex - _ringResolution + 1;
+                _triangles[triIndex + 2] = vertIndex + 1;
+
+                _triangles[triIndex + 3] = vertIndex;
+                _triangles[triIndex + 4] = vertIndex - _ringResolution;
+                _triangles[triIndex + 5] = vertIndex - _ringResolution + 1;
+
+                vertIndex++;
+                triIndex += 6;
+            }
+
+            _triangles[triIndex] = vertIndex;
+            _triangles[triIndex + 1] = vertIndex - 2 * _ringResolution + 1;
+            _triangles[triIndex + 2] = vertIndex - _ringResolution + 1;
+
+            _triangles[triIndex + 3] = vertIndex;
+            _triangles[triIndex + 4] = vertIndex - _ringResolution;
+            _triangles[triIndex + 5] = vertIndex - 2 * _ringResolution + 1;
+        }
+
+        _ropeMesh.Clear();
+        _ropeMesh.SetVertices(_vertices);
+        _ropeMesh.SetTriangles(_triangles, 0);
+        _ropeMesh.RecalculateNormals();
+    }
+}
