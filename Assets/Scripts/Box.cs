057ca08b1cfe3f77463e888ce8f90491183bf19b
Alfred <59073967+slimer37@users.noreply.github.com>
2022-12-31 17:11:15 -0800

    Full box storage

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Box : Interactable
 {
@@ -14,41 +15,135 @@ public class Box : Interactable
     [SerializeField] Vector3 _randomRotation;
     [SerializeField] int _maxRotations;
 
-    List<GameObject> _contents;
+    [Header("Interface")]
+    [SerializeField] InputAction _decreaseIndex;
+    [SerializeField] InputAction _increaseIndex;
+    [SerializeField] string instructions;
+
+    List<Item> _contents;
+
+    int _selectedItemIndex;
 
     void Awake()
     {
         _levelIndicator.gameObject.SetActive(false);
-        _contents = new List<GameObject>();
+        _contents = new List<Item>();
+
+        _increaseIndex.performed += IncreaseIndex;
+        _decreaseIndex.performed += DecreaseIndex;
     }
 
-    protected override bool CanInteract(Employee e) =>
-        _contents.Count < _capacity && e.RightHand.IsFull;
+    void IncreaseIndex(InputAction.CallbackContext obj) =>
+        _selectedItemIndex = (_selectedItemIndex + 1) % _contents.Count;
+
+    void DecreaseIndex(InputAction.CallbackContext obj) =>
+        _selectedItemIndex = (_contents.Count + _selectedItemIndex - 1) % _contents.Count;
+
+    public override void OnHover(bool hover)
+    {
+        if (hover)
+        {
+            _decreaseIndex.Enable();
+            _increaseIndex.Enable();
+        }
+        else
+        {
+            _decreaseIndex.Disable();
+            _increaseIndex.Disable();
+        }
+    }
 
     public override HudInfo GetHudInfo(Employee e)
     {
-        return CanInteract(e)
-            ? new()
-            {
-                icon = Icon.Hand,
-                text = $"Store {e.RightHand.HeldObject.name}\n{_contents.Count}/{_capacity}"
-            }
-            : BlankHud;
+        if (!CanInteract(e)) return BlankHud;
+
+        var isStoringItem = e.RightHand.IsFull;
+
+        if (isStoringItem && _contents.Count == _capacity)
+        {
+            return new() { icon = Icon.Invalid, text = "Box is full" };
+        }
+
+        if (!isStoringItem && _contents.Count == 0)
+        {
+            return new() { icon = Icon.Invalid, text = "Box is empty" };
+        }
+
+        var hud = new HudInfo() { icon = Icon.Hand };
+
+        var spacesLeft = _capacity - _contents.Count;
+        var spacesText = $"{spacesLeft} space" + (spacesLeft > 1 ? "s" : "");
+
+        if (isStoringItem)
+        {
+            hud.text = $"Store {e.RightHand.HeldObject.name}\n{spacesText} left";
+        }
+        else if (_contents.Count == 1)
+        {
+            hud.text = $"Take {_contents[0].name}";
+        }
+        else
+        {
+            var index = _selectedItemIndex;
+            hud.text = $"Take {_contents[index].name}\n{index + 1}/{_contents.Count}\n" + instructions;
+        }
+
+        return hud;
     }
 
     protected override void OnInteract(Employee e)
     {
-        e.ItemHotbar.TryRemoveActiveItem(out var item);
+        var isStoringItem = e.RightHand.IsFull;
 
-        var obj = item.gameObject;
+        if (isStoringItem)
+        {
+            if (_contents.Count == _capacity) return;
 
-        _contents.Add(obj);
-        obj.SetActive(false);
+            e.ItemHotbar.TryRemoveActiveItem(out var item);
 
+            StoreItem(item);
+        }
+        else
+        {
+            if (_contents.Count == 0) return;
+
+            RetrieveItem(e.ItemHotbar);
+        }
+
+        ClampSelectedIndex();
+
+        UpdateLevel();
+    }
+
+    void StoreItem(Item item)
+    {
+        _contents.Add(item);
+
+        item.gameObject.SetActive(false);
+    }
+
+    void RetrieveItem(Hotbar hotbar)
+    {
+        var item = _contents[_selectedItemIndex];
+
+        item.transform.position = transform.position;
+
+        hotbar.TryAddItem(item);
+
+        _contents.RemoveAt(_selectedItemIndex);
+    }
+
+    void UpdateLevel()
+    {
         _levelIndicator.gameObject.SetActive(_contents.Count > 0);
 
         var filledAmount = (float)_contents.Count / _capacity;
         _levelIndicator.localPosition = Vector3.up * Mathf.Lerp(_lowestY, _highestY, filledAmount);
         _levelIndicator.localEulerAngles = _randomRotation * Random.Range(0, _maxRotations);
+    }
+
+    void ClampSelectedIndex()
+    {
+        _selectedItemIndex = Mathf.Clamp(_selectedItemIndex, 0, _contents.Count - 1);
     }
 }
