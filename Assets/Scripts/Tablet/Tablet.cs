798630708ef8138367609dccae81949125dee134
Alfred <59073967+slimer37@users.noreply.github.com>
2023-01-06 18:14:09 -0800

    Add namespace to tablet

--- Assets/Scripts/Tablet/Tablet.cs
+++ Assets/Scripts/Tablet/Tablet.cs
@@ -2,118 +2,121 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Tablet : MonoBehaviour
+namespace Recounter.Tablet
 {
-    [SerializeField] Canvas _canvas;
-    [SerializeField] CanvasGroup _canvasGroup;
-    [SerializeField] float _canvasFadeTime;
-    [SerializeField] PlayerController _controller;
-    [SerializeField] PlayerInteraction _interaction;
-
-    [Header("Hands Full Check")]
-    [SerializeField] Employee _employee;
-    [SerializeField] Canvas _messageCanvas;
-    [SerializeField] CanvasGroup _messageUI;
-    [SerializeField] float _messageExpiry;
-    [SerializeField] float _messageFadeTime;
-
-    [Header("Physical Tablet")]
-    [SerializeField] Transform _physicalTablet;
-
-    [Header("Raise Animation")]
-    [SerializeField] float _raiseTime;
-    [SerializeField] Vector3 _raisedPosition;
-    [SerializeField] Vector3 _raisedRotation;
-    [SerializeField] Ease _ease;
-
-    Tween _currentTween;
-
-    bool _isUp;
-    InputAction _openTabletAction;
-
-    Vector3 _loweredPosition;
-    Vector3 _loweredRotation;
-
-    void Awake()
+    public class Tablet : MonoBehaviour
     {
-        _canvas.enabled = false;
-        _messageCanvas.enabled = false;
+        [SerializeField] Canvas _canvas;
+        [SerializeField] CanvasGroup _canvasGroup;
+        [SerializeField] float _canvasFadeTime;
+        [SerializeField] PlayerController _controller;
+        [SerializeField] PlayerInteraction _interaction;
 
-        _openTabletAction = new Controls().Player.OpenTablet;
-        _openTabletAction.performed += _ => OpenTablet();
+        [Header("Hands Full Check")]
+        [SerializeField] Employee _employee;
+        [SerializeField] Canvas _messageCanvas;
+        [SerializeField] CanvasGroup _messageUI;
+        [SerializeField] float _messageExpiry;
+        [SerializeField] float _messageFadeTime;
 
-        _loweredPosition = _physicalTablet.localPosition;
-        _loweredRotation = _physicalTablet.eulerAngles;
-    }
+        [Header("Physical Tablet")]
+        [SerializeField] Transform _physicalTablet;
 
-    void OnEnable() => _openTabletAction.Enable();
-    void OnDisable() => _openTabletAction.Disable();
+        [Header("Raise Animation")]
+        [SerializeField] float _raiseTime;
+        [SerializeField] Vector3 _raisedPosition;
+        [SerializeField] Vector3 _raisedRotation;
+        [SerializeField] Ease _ease;
 
-    void OpenTablet()
-    {
-        if (Pause.IsPaused || _currentTween.IsActive() && _currentTween.IsPlaying()) return;
+        Tween _currentTween;
 
-        CancelInvoke();
+        bool _isUp;
+        InputAction _openTabletAction;
 
-        if (!_employee.HandsAreFree)
+        Vector3 _loweredPosition;
+        Vector3 _loweredRotation;
+
+        void Awake()
         {
-            _messageCanvas.enabled = true;
-            _messageUI.alpha = 1;
-            GetFadeTween().SetDelay(_messageExpiry);
-            return;
-        }
-
-        _isUp = !_isUp;
-
-        _currentTween = DOTween.Sequence()
-            .Append(_physicalTablet.DOLocalMove(_isUp ? _raisedPosition : _loweredPosition, _raiseTime).SetEase(_ease))
-            .Join(_physicalTablet.DOLocalRotate(_isUp ? _raisedRotation : _loweredRotation, _raiseTime).SetEase(_ease));
-
-        if (_isUp)
-        {
-            ConfigurePlayerComponents(true);
-
-            _currentTween.OnComplete(() => ShowUI(true));
-        }
-        else
-        {
-            ConfigurePlayerComponents(false);
-            ShowUI(false);
-        }
-    }
-
-    void ConfigurePlayerComponents(bool tabletOpen)
-    {
-        _controller.Suspend(tabletOpen, true);
-        _interaction.enabled = !tabletOpen;
-        _employee.ShowHud(!tabletOpen);
-    }
-
-    void ShowUI(bool show)
-    {
-        _canvas.enabled = true;
-
-        _canvasGroup.DOKill();
-
-        var tween = _canvasGroup.DOFade(show ? 1 : 0, _canvasFadeTime);
-
-        if (show)
-        {
-            // Reset message
-
+            _canvas.enabled = false;
             _messageCanvas.enabled = false;
-            _messageUI.DOKill();
 
-            return;
+            _openTabletAction = new Controls().Player.OpenTablet;
+            _openTabletAction.performed += _ => OpenTablet();
+
+            _loweredPosition = _physicalTablet.localPosition;
+            _loweredRotation = _physicalTablet.eulerAngles;
         }
 
-        tween.OnComplete(() => _canvas.enabled = false);
-    }
+        void OnEnable() => _openTabletAction.Enable();
+        void OnDisable() => _openTabletAction.Disable();
 
-    Tween GetFadeTween()
-    {
-        _messageUI.DOKill();
-        return _messageUI.DOFade(0, _messageFadeTime)
-            .OnComplete(() => _messageCanvas.enabled = false);
+        void OpenTablet()
+        {
+            if (Pause.IsPaused || _currentTween.IsActive() && _currentTween.IsPlaying()) return;
+
+            CancelInvoke();
+
+            if (!_employee.HandsAreFree)
+            {
+                _messageCanvas.enabled = true;
+                _messageUI.alpha = 1;
+                GetFadeTween().SetDelay(_messageExpiry);
+                return;
+            }
+
+            _isUp = !_isUp;
+
+            _currentTween = DOTween.Sequence()
+                .Append(_physicalTablet.DOLocalMove(_isUp ? _raisedPosition : _loweredPosition, _raiseTime).SetEase(_ease))
+                .Join(_physicalTablet.DOLocalRotate(_isUp ? _raisedRotation : _loweredRotation, _raiseTime).SetEase(_ease));
+
+            if (_isUp)
+            {
+                ConfigurePlayerComponents(true);
+
+                _currentTween.OnComplete(() => ShowUI(true));
+            }
+            else
+            {
+                ConfigurePlayerComponents(false);
+                ShowUI(false);
+            }
+        }
+
+        void ConfigurePlayerComponents(bool tabletOpen)
+        {
+            _controller.Suspend(tabletOpen, true);
+            _interaction.enabled = !tabletOpen;
+            _employee.ShowHud(!tabletOpen);
+        }
+
+        void ShowUI(bool show)
+        {
+            _canvas.enabled = true;
+
+            _canvasGroup.DOKill();
+
+            var tween = _canvasGroup.DOFade(show ? 1 : 0, _canvasFadeTime);
+
+            if (show)
+            {
+                // Reset message
+
+                _messageCanvas.enabled = false;
+                _messageUI.DOKill();
+
+                return;
+            }
+
+            tween.OnComplete(() => _canvas.enabled = false);
+        }
+
+        Tween GetFadeTween()
+        {
+            _messageUI.DOKill();
+            return _messageUI.DOFade(0, _messageFadeTime)
+                .OnComplete(() => _messageCanvas.enabled = false);
+        }
     }
 }
