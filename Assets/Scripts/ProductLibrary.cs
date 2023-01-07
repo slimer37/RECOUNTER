2ee243b26fef36ce04bcb0694fc0b7985eee08b4
Alfred <59073967+slimer37@users.noreply.github.com>
2023-01-06 18:13:22 -0800

    Create ProductLibrary SO w/ ink cartridge only

---
+++ Assets/Scripts/ProductLibrary.cs
@@ -0,0 +1,31 @@
+﻿using NaughtyAttributes;
+using System;
+using System.Globalization;
+using UnityEngine;
+
+namespace Recounter.Inventory
+{
+    [CreateAssetMenu(menuName = "Products/Product Library")]
+    public class ProductLibrary : ScriptableObject
+    {
+        [SerializeField] Product[] _products;
+
+        public Product[] Products => _products;
+    }
+
+    [Serializable]
+    public class Product
+    {
+        [SerializeField] string _displayName;
+        [SerializeField, AllowNesting, Label("Price ($)")] float _price;
+        [SerializeField, TextArea(3, 5)] string _description;
+        [SerializeField, ShowAssetPreview] GameObject _prefab;
+
+        static readonly CultureInfo EnUsCulture = new("en-US");
+
+        public string DisplayName => _displayName;
+        public string Description => _description;
+        public float Price => _price;
+        public string FormattedPrice => _price.ToString("C", EnUsCulture);
+    }
+}
