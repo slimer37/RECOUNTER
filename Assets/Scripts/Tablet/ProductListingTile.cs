e09cedb622558497156e584768c5fcf1913464e2
Alfred <59073967+slimer37@users.noreply.github.com>
2023-01-11 23:05:17 -0800

    Implement product listing screen

--- Assets/Scripts/Tablet/ProductListingTile.cs
+++ Assets/Scripts/Tablet/ProductListingTile.cs
@@ -12,8 +12,14 @@ namespace Recounter.Tablet
         [SerializeField] TextMeshProUGUI _priceText;
         [SerializeField] RawImage _image;
 
+        Product _product;
+
+        public void OpenListing() => _listingUI.Open(_product);
+
         public void InitializeToProduct(Product product)
         {
+            _product = product;
+
             _nameText.text = product.DisplayName;
             _priceText.text = product.FormattedPrice;
             _image.texture = product.Thumbnail;
--- Assets/Scripts/Tablet/ProductListingUI.cs
+++ Assets/Scripts/Tablet/ProductListingUI.cs
@@ -1,25 +1,35 @@
 ﻿using Recounter.Inventory;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Recounter.Tablet
 {
     public class ProductListingUI : MonoBehaviour
     {
-        [Header("Text")]
+        [SerializeField] Canvas _canvas;
         [SerializeField] TextMeshProUGUI _name;
         [SerializeField] TextMeshProUGUI _price;
         [SerializeField] TextMeshProUGUI _description;
+        [SerializeField] RawImage _image;
 
         Product _focusedProduct;
 
-        void Open(Product product)
+        void Awake()
         {
+            _canvas.enabled = false;
+        }
+
+        public void Open(Product product)
+        {
+            _canvas.enabled = true;
+
             _focusedProduct = product;
 
             _name.text = product.DisplayName;
             _price.text = product.FormattedPrice;
             _description.text = product.Description;
+            _image.texture = product.Thumbnail;
         }
     }
 }
