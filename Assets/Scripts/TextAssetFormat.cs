b93b280a5e0b53cf659e99a1c6e8622c5a470427
Alfred <59073967+slimer37@users.noreply.github.com>
2023-01-13 00:26:13 -0800

    Make formatter to create credits from file

---
+++ Assets/Scripts/TextAssetFormat.cs
@@ -0,0 +1,39 @@
+﻿using NaughtyAttributes;
+using System;
+using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine;
+
+namespace Recounter.UI
+{
+    public class TextAssetFormat : MonoBehaviour
+    {
+        [Serializable]
+        struct RegexReplacement
+        {
+            public string regex;
+            public string replacement;
+        }
+
+        [SerializeField] TextAsset _asset;
+        [SerializeField] TMP_Text _text;
+        [SerializeField] RegexReplacement[] _replacements;
+
+        [Button("Test Regex")]
+        void Test() => print(FormatTextAsset());
+
+        void Awake() => _text.text = FormatTextAsset();
+
+        string FormatTextAsset()
+        {
+            var formatted = _asset.text;
+
+            foreach (var r in _replacements)
+            {
+                formatted = Regex.Replace(formatted, r.regex, r.replacement);
+            }
+
+            return formatted;
+        }
+    }
+}
