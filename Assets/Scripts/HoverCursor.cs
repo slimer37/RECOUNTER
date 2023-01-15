cf572c7b5930de052a82f6cf36f8e82ccfb93e52
Alfred <59073967+slimer37@users.noreply.github.com>
2023-01-15 00:30:06 -0800

    Link cursor when hovering over selectables

---
+++ Assets/Scripts/HoverCursor.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.UI;
+
+public class HoverCursor : MonoBehaviour
+{
+    [SerializeField] Texture2D _cursor;
+    [SerializeField] Vector2 _hotspot;
+
+    GameObject _lastHover;
+
+    bool _cursorActive;
+
+    void Update()
+    {
+        var module = EventSystem.current.currentInputModule as InputSystemUIInputModule;
+        var result = module.GetLastRaycastResult(0).gameObject;
+
+        if (_lastHover == result) return;
+
+        _lastHover = result;
+
+        if (!result)
+        {
+            Clear();
+            return;
+        }
+
+        OnNewHoverGained(result);
+    }
+
+    void OnNewHoverGained(GameObject hover)
+    {
+        if (!hover.GetComponentInParent<Selectable>())
+        {
+            Clear();
+            return;
+        }
+
+        Use();
+    }
+
+    void Use()
+    {
+        if (_cursorActive) return;
+
+        Cursor.SetCursor(_cursor, _hotspot, CursorMode.Auto);
+        _cursorActive = true;
+    }
+
+    void Clear()
+    {
+        if (!_cursorActive) return;
+
+        Cursor.SetCursor(null, Vector2.one, CursorMode.Auto);
+        _cursorActive = false;
+    }
+}
