1f88c85004672df65fb9a6870c5292476e988ebe
Alfred <59073967+slimer37@users.noreply.github.com>
2023-01-15 22:12:36 -0800

    Add empty search text

--- Assets/Scripts/Tablet/MarketplaceUI.cs
+++ Assets/Scripts/Tablet/MarketplaceUI.cs
@@ -15,6 +15,7 @@ namespace Recounter.Tablet
         [SerializeField] TMP_InputField _searchField;
         [SerializeField] Button _searchButton;
         [SerializeField] Page _page;
+        [SerializeField] GameObject _emptySearchMessage;
 
         ProductListingTile[] tiles;
 
@@ -35,11 +36,18 @@ namespace Recounter.Tablet
 
             query = query.ToLowerInvariant();
 
+            var noneFound = true;
+
             foreach (var tile in tiles)
             {
                 var match = tile.Product.DisplayName.ToLowerInvariant().Contains(query);
                 tile.gameObject.SetActive(match);
+
+                if (match)
+                    noneFound = false;
             }
+
+            _emptySearchMessage.SetActive(noneFound);
         }
 
         void InitializeTiles()
