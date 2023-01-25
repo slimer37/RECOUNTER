2bafbe2238cce8ab096690fbf16fe820ae80f3a1
Alfred <59073967+slimer37@users.noreply.github.com>
2023-01-24 22:06:56 -0800

    Update slosh code

--- Assets/Scripts/Slosh.cs
+++ Assets/Scripts/Slosh.cs
@@ -3,63 +3,53 @@ using UnityEngine;
 public class Slosh : MonoBehaviour
 {
     public int MaterialIndex = 0;
-    public float MaxWobble = 0.15f;
+    public float MaxWobble = 0.5f;
     public float WobbleSpeed = 3f;
-    public float Recovery = 5f;
+    public float Recovery = 3f;
 
     float pulse;
-    float time = 0.5f;
 
     Material material;
     Vector3 lastPos;
-    Vector3 velocity;
     Vector3 lastRot;
-    Vector3 angularVelocity;
 
-    Vector3 wobbleAmount;
-    Vector3 wobbleAmountToAdd;
+    Vector3 wobbleVector;
+
+    int wobbleXId;
+    int wobbleZId;
 
     void Start()
     {
         material = GetComponent<Renderer>().materials[MaterialIndex];
+        pulse = 2 * Mathf.PI * WobbleSpeed;
+
+        wobbleXId = Shader.PropertyToID("_WobbleX");
+        wobbleZId = Shader.PropertyToID("_WobbleZ");
     }
 
     void Update()
     {
-        if (float.IsNaN(wobbleAmountToAdd.x))
-        {
-            wobbleAmountToAdd = Vector3.zero;
-        }
-
-        time += Time.deltaTime;
-
-        // decrease wobble over time
-        wobbleAmountToAdd = Vector3.Lerp(wobbleAmountToAdd, Vector3.zero, Time.deltaTime * Recovery);
-
-        // make a sine wave of the decreasing wobble
-        pulse = 2 * Mathf.PI * WobbleSpeed;
-        wobbleAmount = wobbleAmountToAdd * Mathf.Sin(pulse * time);
-
-        // send it to the shader
-        material.SetFloat("_WobbleX", Vector3.Dot(wobbleAmount, transform.forward));
-        material.SetFloat("_WobbleZ", Vector3.Dot(wobbleAmount, transform.right));
-
-        // velocity
-        velocity = (lastPos - transform.position) / Time.deltaTime;
-
         var currentAngles = transform.eulerAngles;
 
-        for (var i = 0; i < 3; i++)
-        {
-            angularVelocity[i] = Mathf.DeltaAngle(currentAngles[i], lastRot[i]);
-        }
+        var angularVelocity = Vector3.zero;
+
+        angularVelocity.x = Mathf.DeltaAngle(currentAngles.z, lastRot.z);
+        angularVelocity.z = Mathf.DeltaAngle(currentAngles.x, lastRot.x);
+
+        var deltaPosition = lastPos - transform.position;
 
         // add clamped velocity to wobble
-        wobbleAmountToAdd += Vector3.ClampMagnitude(
-            0.01f * MaxWobble * (velocity + new Vector3(angularVelocity.z, 0, angularVelocity.x)),
+        wobbleVector += Vector3.ClampMagnitude(
+            MaxWobble * (deltaPosition + Time.deltaTime * angularVelocity),
             MaxWobble);
 
-        // keep last position
+        wobbleVector = Vector3.Lerp(wobbleVector, Vector3.zero, Time.deltaTime * Recovery);
+
+        var wobbleAmount = wobbleVector * Mathf.Sin(pulse * Time.time);
+
+        material.SetFloat(wobbleXId, Vector3.Dot(wobbleAmount, transform.forward));
+        material.SetFloat(wobbleZId, Vector3.Dot(wobbleAmount, transform.right));
+
         lastPos = transform.position;
         lastRot = transform.rotation.eulerAngles;
     }
