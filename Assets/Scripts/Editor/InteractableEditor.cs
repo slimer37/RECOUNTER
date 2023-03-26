b4f435056c78d7152bc084e5b5010f11c18797c5
Alfred <59073967+slimer37@users.noreply.github.com>
2023-03-26 01:06:53 -0700

    Create button prompt to set Interactable layer

---
+++ Assets/Scripts/Editor/InteractableEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Interactable), true)]
+public class InteractableEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var go = (target as Interactable).gameObject;
+        var interactableLayer = LayerMask.NameToLayer("Interactable");
+
+        if (go.layer != interactableLayer)
+        {
+            EditorGUILayout.HelpBox("Object has the wrong layer.", MessageType.Warning);
+
+            if (GUILayout.Button("Fix", GUILayout.Height(30)))
+            {
+                Undo.RecordObject(go, "Fix layer on interactable");
+                go.layer = interactableLayer;
+            }
+        }
+
+        base.OnInspectorGUI();
+    }
+}
