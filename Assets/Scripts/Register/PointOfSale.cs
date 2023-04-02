9938c29318d288b68b08d5989b1a873a770c2c95
Alfred <59073967+slimer37@users.noreply.github.com>
2023-04-02 12:50:39 -0700

    Discount separate listing

--- Assets/Scripts/Register/PointOfSale.cs
+++ Assets/Scripts/Register/PointOfSale.cs
@@ -14,7 +14,8 @@ namespace Recounter.Service
         // Receipt printing
 
         [SerializeField] TMP_Text _totalInfo;
-        [SerializeField, TextArea, RequireSubstring("{0}", "{1}")] string _totalFormat;
+        [SerializeField, TextArea] string _totalFormat = "Sub-total: {0:C}\n\n{1}\n\nTotal: {2:C}";
+        [SerializeField, TextArea] string _extraInfoFormat = "Discount: {0:P2}";
 
         [SerializeReference] ProductEntryModule _productEntryModule;
 
@@ -22,6 +23,9 @@ namespace Recounter.Service
         [SerializeField] Transform _listParent;
 
         Transaction _currentTransaction;
+
+        float _discount;
+        float _total;
 
         void OnValidate()
         {
@@ -59,9 +63,18 @@ namespace Recounter.Service
             Instantiate(_lineItemPrefab, _listParent).PopulateInfo(lineItem);
         }
 
-        void UpdateTotal(float total)
+        void UpdateTotal(float subtotal)
         {
-            _totalInfo.text = string.Format(_totalFormat, total, total);
+            _total = subtotal * (1 - _discount);
+
+            var extraInfo = "";
+
+            if (_discount > 0)
+            {
+                extraInfo = string.Format(_extraInfoFormat, _discount);
+            }
+
+            _totalInfo.text = string.Format(_totalFormat, subtotal, extraInfo, _total);
         }
     }
 }
