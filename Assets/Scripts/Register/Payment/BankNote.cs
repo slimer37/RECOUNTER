f7f3d27da245cdb0b0aea1a91d11f869a8444cd7
Alfred <59073967+slimer37@users.noreply.github.com>
2023-04-08 02:02:38 -0700

    Basic bank note and drawer slot

---
+++ Assets/Scripts/Register/Payment/BankNote.cs
@@ -0,0 +1,75 @@
+using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Recounter
+{
+    public class BankNote : Interactable
+    {
+        [field: SerializeField] public float Value { get; private set; }
+
+        public const float Spacing = 0.01f;
+
+        Stack<BankNote> _stackedNotes = new();
+
+        Collider _collider;
+
+        Hand _hand;
+
+        void Awake()
+        {
+            _collider = GetComponent<Collider>();
+        }
+
+        protected override bool CanInteract(Employee e) => !e.LeftHand.IsFull || e.LeftHand.Contains<BankNote>(out _);
+
+        protected override void OnInteract(Employee e)
+        {
+            _collider.enabled = false;
+
+            if (e.LeftHand.Contains<BankNote>(out var note))
+            {
+                note.Stack(this);
+            }
+            else
+            {
+                _hand = e.LeftHand;
+                _hand.Hold(this);
+            }
+        }
+
+        void Stack(BankNote note)
+        {
+            _hand.Clear();
+
+            note.transform.parent = transform;
+
+            _hand.Hold(this);
+
+            note.transform.DOLocalMove(Vector3.up * Spacing, 0.1f);
+            note.transform.DOLocalRotate(Vector3.up * Random.Range(-5f, 5f), 0.1f);
+
+            _stackedNotes.Push(note);
+        }
+
+        public BankNote RetrieveFromStack()
+        {
+            if (_stackedNotes.Count == 0)
+            {
+                _hand.Clear();
+                _hand = null;
+                return this;
+            }
+
+            var note = _stackedNotes.Pop();
+
+            _hand.Clear();
+
+            note.transform.parent = null;
+
+            _hand.Hold(this);
+
+            return note;
+        }
+    }
+}
---
+++ Assets/Scripts/Register/Payment/CurrencySlot.cs
@@ -0,0 +1,129 @@
+using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Recounter
+{
+    public class CurrencySlot : Interactable
+    {
+        [SerializeField] float _moveTime;
+        [SerializeField] Vector3 _rotation;
+        [SerializeField] float _holdThreshold = 0.15f;
+
+        [Header("Lever Angle Calculation")]
+        [SerializeField] Transform _lever;
+        [SerializeField] float _leverLength = 0.203f;
+        [SerializeField] float _leverRestOffset;
+        [SerializeField] float _leverAngleOffset;
+        [SerializeField] Vector3 _leverAxis;
+        [SerializeField] float _leverOpenAngle;
+
+        [Header("Lever Return Animation")]
+        [SerializeField] float _leverReturnTime;
+        [SerializeField] Ease _leverReturnEase;
+
+        readonly Stack<BankNote> _notes = new();
+
+        float _holdTime;
+
+        bool _holding;
+
+        float _leverClosedAngle;
+
+        float GetClosedAngle()
+        {
+            var baseHeight = transform.position.y + _notes.Count * BankNote.Spacing;
+            var h = _lever.position.y - baseHeight + _leverRestOffset;
+            return Mathf.Acos(h / _leverLength) * Mathf.Rad2Deg + _leverAngleOffset;
+        }
+
+        void Awake()
+        {
+            foreach (var note in GetComponentsInChildren<BankNote>())
+            {
+                StoreNote(note);
+            }
+
+            _lever.localEulerAngles = _leverAxis * GetClosedAngle();
+        }
+
+        protected override void OnInteract(Employee e)
+        {
+            _lever.DOKill();
+
+            _holdTime = 0;
+            _holding = true;
+
+            _leverClosedAngle = GetClosedAngle();
+        }
+
+        void StoreNote(BankNote note, bool direct = false)
+        {
+            _notes.Push(note);
+
+            var noteTransform = note.transform;
+
+            noteTransform.parent = transform;
+
+            var localPos = Vector3.up * (BankNote.Spacing * _notes.Count);
+            var localRot = _rotation;
+
+            if (direct)
+            {
+                noteTransform.localPosition = localPos;
+                noteTransform.localEulerAngles = _rotation;
+                return;
+            }
+
+            noteTransform.DOLocalMove(localPos, _moveTime);
+            noteTransform.DOLocalRotate(localRot, _moveTime);
+        }
+
+        void GrabNote(Employee e)
+        {
+            var note = _notes.Pop();
+
+            note.transform.DOKill();
+            note.Interact(e);
+        }
+
+        void Update()
+        {
+            if (_holding)
+            {
+                _holdTime += Time.deltaTime;
+                _lever.localEulerAngles = _leverAxis * Mathf.LerpAngle(_leverClosedAngle, _leverOpenAngle, _holdTime / _holdThreshold);
+
+                if (_holdTime > _holdThreshold)
+                {
+                    AttemptToStoreNote();
+                    _holding = false;
+                }
+            }
+        }
+
+        void AttemptToStoreNote()
+        {
+            if (Interactor.LeftHand.Contains<BankNote>(out var note))
+            {
+                StoreNote(note.RetrieveFromStack());
+                _leverClosedAngle = GetClosedAngle();
+            }
+        }
+
+        protected override void OnEndInteraction()
+        {
+            _lever.DOLocalRotate(_leverAxis * _leverClosedAngle, _leverReturnTime)
+                .SetEase(_leverReturnEase);
+
+            if (!_holding) return;
+            
+            if (_notes.Count > 0)
+            {
+                GrabNote(Interactor);
+            }
+
+            _holding = false;
+        }
+    }
+}
