30e1c6c60c6c5f781632e2fe7c8db9684fad7673
Alfred <59073967+slimer37@users.noreply.github.com>
2023-05-01 17:44:37 -0700

    Rail placement for clothing rack

---
+++ Assets/Scripts/Placement/PlacementMethods/RailPlacement.cs
@@ -0,0 +1,83 @@
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Recounter
+{
+    public class RailPlacement : MonoBehaviour, IPlacementMethod
+    {
+        [SerializeField, Tag] string _filterTag;
+        [SerializeField] Transform _initial;
+        [SerializeField] float _speed;
+        [SerializeField] float _modifierFactor;
+        [SerializeField] float _extent;
+        [SerializeField] float _yRotOffset;
+        [SerializeField] LayerMask _obstacleMask;
+
+        Placer _placer;
+        Transform _body;
+        Transform _camera;
+
+        Plane _placementPlane;
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.matrix = _initial.localToWorldMatrix;
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(Vector3.left * _extent, Vector3.right * _extent);
+        }
+
+        void Awake()
+        {
+            _placementPlane = new Plane(_initial.forward, _initial.position);
+        }
+
+        public bool Accepts(Item item) => item.gameObject.CompareTag(_filterTag);
+
+        public void SetUp(Placer placer, Transform body, Transform camera)
+        {
+            _placer = placer;
+            _body = body;
+            _camera = camera;
+        }
+
+        Vector3 ConstrainToRail(Vector3 point)
+        {
+            var localPoint = _initial.InverseTransformPoint(point);
+
+            localPoint.x = Mathf.Clamp(localPoint.x, -_extent, _extent);
+
+            localPoint.y = _placer.Active.SizeAlong(Vector3.down);
+
+            localPoint.z = 0;
+
+            return _initial.TransformPoint(localPoint);
+        }
+
+        public void GetInitialPositionAndRotation(out Vector3 position, out Vector3 eulerAngles)
+        {
+            var ray = new Ray(_camera.position, _camera.forward);
+
+            _placementPlane.Raycast(ray, out var enter);
+
+            position = ConstrainToRail(ray.GetPoint(enter));
+
+            var facing = (Mathf.Sign(Vector3.Dot(_initial.forward, _camera.forward)) + 1) / 2;
+
+            eulerAngles = _initial.eulerAngles + (180f * facing + _yRotOffset) * Vector3.up;
+        }
+
+        public void HandlePlacement(ref Vector3 placePosition, ref Vector3 placeRotation, bool modifier, Vector2 mouseDelta, float rawScroll, out PlacementCursor cursor)
+        {
+            var delta = _body.TransformDirection(new Vector3(mouseDelta.x, 0, mouseDelta.y));
+
+            delta *= _speed * (modifier ? _modifierFactor : 1f);
+
+            placePosition = ConstrainToRail(placePosition + delta);
+
+            cursor = PlacementCursor.Placement;
+        }
+
+        public bool IsItemPositionValid(Vector3 position, Quaternion rotation) =>
+            !_placer.Active.WouldIntersectAt(position, rotation, _obstacleMask);
+    }
+}
