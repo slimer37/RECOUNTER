ba669fb56c4fdc10b333b6c7eda9a8bfe865b4e4
Alfred <59073967+slimer37@users.noreply.github.com>
2023-08-02 16:18:00 -0700

    Fix load save menu not clearing old list items

--- Assets/Scripts/Store/SaveFileUI/LoadSaveMenu.cs
+++ Assets/Scripts/Store/SaveFileUI/LoadSaveMenu.cs
@@ -1,4 +1,5 @@
 using Recounter.Store;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Recounter.UI
@@ -9,6 +10,8 @@ namespace Recounter.UI
         [SerializeField] SaveListItem _listItemPrefab;
         [SerializeField] MenuEscape _menuEscape;
 
+        readonly List<SaveListItem> _listItems = new();
+
         void Awake()
         {
             _menuEscape.Opened += LoadSaves;
@@ -16,6 +19,13 @@ namespace Recounter.UI
 
         void LoadSaves()
         {
+            foreach (var item in _listItems)
+            {
+                Destroy(item.gameObject);
+            }
+
+            _listItems.Clear();
+
             _listItemPrefab.gameObject.SetActive(true);
 
             foreach (var fileName in StoreSerializer.AllSaveFiles())
@@ -25,6 +35,8 @@ namespace Recounter.UI
                 var clone = Instantiate(_listItemPrefab, _parent);
 
                 clone.Populate(storeData);
+
+                _listItems.Add(clone);
             }
 
             _listItemPrefab.gameObject.SetActive(false);
