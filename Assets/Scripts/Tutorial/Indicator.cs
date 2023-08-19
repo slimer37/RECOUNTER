ded369b5b590c4c4f6907f4cd25083ab78b52589
Alfred <59073967+slimer37@users.noreply.github.com>
2023-08-18 22:29:19 -0700

    Offscreen arrow

--- Assets/Scripts/Tutorial/Indicator.cs
+++ Assets/Scripts/Tutorial/Indicator.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Recounter.Tutorial
 {
     public class Indicator : MonoBehaviour
     {
+        [SerializeField] float _bounceSpeed;
         [SerializeField] float _offset;
+        [SerializeField] Renderer _renderer;
+
+        [Header("Screen Arrow")]
+        [SerializeField] Image _uiArrow;
+        [SerializeField] float _arrowBounce;
+        [SerializeField] float _margin;
 
         static Indicator s_instance;
+
+        Vector3 _target;
+
+        bool _active;
+
+        Camera _camera;
 
         void Awake()
         {
@@ -16,28 +30,88 @@ namespace Recounter.Tutorial
             s_instance = this;
 
             gameObject.SetActive(false);
+
+            _camera = Camera.main;
+
+            Begin(transform.position);
         }
 
-        public static void Indicate(Vector3 target)
+        float GetOffset() => Mathf.Abs(Mathf.Sin(Mathf.PI / 2 * Time.time * _bounceSpeed));
+
+        public static void BeginIndicate(Vector3 target)
         {
-            s_instance.ShowAt(target);
+            s_instance.Begin(target);
         }
 
-        public static void Hide()
+        public static void EndIndicate()
         {
-            s_instance.gameObject.SetActive(false);
+            s_instance.End();
         }
 
-        void ShowAt(Vector3 target)
+        void Begin(Vector3 target)
         {
-            transform.position = target + Vector3.up * _offset;
+            _active = true;
+
+            _target = target;
+
             gameObject.SetActive(true);
+        }
+
+        void End()
+        {
+            _active = false;
+
+            gameObject.SetActive(false);
+
+            _uiArrow.enabled = false;
         }
 
         void Update()
         {
+            if (!_active) return;
+
             transform.Rotate(Time.deltaTime * 90 * Vector3.up);
-            transform.Translate(Vector3.up * Mathf.Cos(Time.time * Mathf.PI) * Time.deltaTime);
+            transform.position = _target + Vector3.up * GetOffset();
+
+            var screenPos = _camera.WorldToViewportPoint(_target);
+
+            var isVisible = Mathf.Abs(screenPos.x - 0.5f) < 0.5f && Mathf.Abs(screenPos.y - 0.5f) < 0.5f;
+
+            _uiArrow.enabled = !isVisible;
+
+            if (!isVisible)
+            {
+                if (screenPos.z < 0)
+                {
+                    screenPos *= -1;
+                }
+
+                var arrowPos = screenPos;
+
+                arrowPos = (arrowPos - Vector3.one / 2) * 2;
+
+                var max = Mathf.Max(Mathf.Abs(arrowPos.x), Mathf.Abs(arrowPos.y));
+
+                arrowPos = arrowPos / (max * 2) + Vector3.one / 2;
+
+                var toTarget = screenPos - arrowPos;
+
+                var angle = Mathf.Atan2(toTarget.y, toTarget.x) * Mathf.Rad2Deg + 90;
+
+                _uiArrow.transform.localEulerAngles = angle * Vector3.forward;
+
+                var screenDimensions = new Vector3(Screen.width, Screen.height);
+
+                var center = screenDimensions / 2;
+
+                arrowPos = Vector3.Scale(arrowPos, screenDimensions);
+
+                var extent = arrowPos - center;
+
+                arrowPos = center + extent * (1 - _margin);
+
+                _uiArrow.transform.position = arrowPos + _uiArrow.transform.up * GetOffset() * _arrowBounce;
+            }
         }
     }
 }
