0eb2e02462b9dd7f024beb2ddb0d4d6ab3f5033b
Alfred <59073967+slimer37@users.noreply.github.com>
2023-08-21 22:53:25 -0700

    Add test tool beep and line enabling

--- Assets/Scripts/Tool/TestTool.cs
+++ Assets/Scripts/Tool/TestTool.cs
@@ -1,3 +1,4 @@
+using FMODUnity;
 using UnityEngine;
 
 namespace Recounter
@@ -5,9 +6,22 @@ namespace Recounter
     public class TestTool : Tool<Transform>
     {
         [SerializeField] LineRenderer _line;
+        [SerializeField] EventReference _beep;
+
+        void Awake()
+        {
+            _line.enabled = false;
+        }
+
+        protected override void OnPickUp()
+        {
+            _line.enabled = true;
+        }
 
         protected override void UseOn(Transform obj)
         {
+            RuntimeManager.PlayOneShot(_beep);
+
             print(obj.name);
 
             _line.SetPosition(0, _line.transform.position);
