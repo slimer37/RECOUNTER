3570112650b94c26c20ac4976ea5f7ed0dc9deae
Alfred <59073967+slimer37@users.noreply.github.com>
2023-09-11 01:17:51 -0700

    Better door
    Shitty commit

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -3,19 +3,21 @@ using UnityEngine;
 
 public class Door : Interactable
 {
-    [SerializeField] float _pullPointDistance;
     [SerializeField] float _pullStrength;
     [SerializeField, Required] Transform _rotator;
     [SerializeField] Vector3 _pullDirection = Vector3.forward;
-    [SerializeField] float _deceleration;
     [SerializeField, Min(0)] float _closeAngle;
     [SerializeField] float _closingForce;
 
     [Header("Colliders")]
-    [SerializeField] Collider _door;
+    [SerializeField, Required] Collider _door;
     [SerializeField] Collider _doorFrame;
 
     HingeJoint _hinge;
+    Rigidbody _rb;
+
+    Vector3 _startPullPoint;
+    float _pullPointDistance;
 
     protected override bool CanInteract(Employee e) => !IsInteractionInProgress;
 
@@ -39,6 +41,7 @@ public class Door : Interactable
 
     void Awake()
     {
+        _rb = GetComponent<Rigidbody>();
         _hinge = GetComponent<HingeJoint>();
 
         if (_door && _doorFrame)
@@ -47,28 +50,39 @@ public class Door : Interactable
         }
     }
 
-    void Update()
+    protected override void OnInteract(Employee e)
     {
-        var rotationVelocity = 0f;
+        var camTransform = e.Camera.transform;
+        _door.Raycast(new Ray(camTransform.position, camTransform.forward), out var hit, 100);
+        _startPullPoint = transform.InverseTransformPoint(hit.point);
+        _hinge.useMotor = false;
+        _pullPointDistance = hit.distance;
+    }
 
+    void FixedUpdate()
+    {
         if (IsInteractionInProgress)
         {
-            var pullPoint = Interactor.transform.TransformPoint(Vector3.forward * _pullPointDistance);
-            var pullDir = _rotator.TransformDirection(_pullDirection);
-            var pull = Vector3.Dot(pullPoint - _rotator.position, pullDir);
+            var pullPoint = Interactor.Camera.transform.TransformPoint(Vector3.forward * _pullPointDistance);
+            var forceTo = transform.InverseTransformPoint(pullPoint);
+            var force = forceTo - _startPullPoint;
+            var moment = Vector3.Cross(_startPullPoint, force);
+            var pull = Vector3.Dot(moment, _hinge.axis) * _pullStrength;
 
-            rotationVelocity = pull * _pullStrength;
+            _rb.AddRelativeTorque(_hinge.axis * pull, ForceMode.Force);
         }
         else if (Mathf.Abs(_hinge.angle) < _closeAngle)
         {
-            rotationVelocity = _closingForce * (_closeAngle - Mathf.Abs(_hinge.angle));
-        }
+            var rotationVelocity = _closingForce * Mathf.Sign(_closeAngle - Mathf.Abs(_hinge.angle));
 
-        Rotate(rotationVelocity);
+            _rb.AddRelativeTorque(_hinge.axis * rotationVelocity, ForceMode.Force);
+        }
     }
 
     void Rotate(float speed)
     {
+        _hinge.useMotor = true;
+        
         var motor = _hinge.motor;
         motor.targetVelocity = speed;
         _hinge.motor = motor;
