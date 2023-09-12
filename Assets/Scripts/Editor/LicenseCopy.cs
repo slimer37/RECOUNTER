1bb2e0c983eabc1b576341ba85b41f95a66ee22a
Alfred <59073967+slimer37@users.noreply.github.com>
2023-09-12 01:40:31 -0700

    Update credits for flood fill and add license copier

---
+++ Assets/Scripts/Editor/LicenseCopy.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace Recounter.Editor
+{
+    public static class LicenseCopy
+    {
+        [PostProcessBuild]
+        public static void OnPostprocessBuild(BuildTarget target, string pathToBuildFile)
+        {
+            File.Copy(Path.Combine(Application.dataPath, "LICENSE.txt"), Path.Combine(pathToBuildFile, "LICENSE.txt"));
+        }
+    }
+}
